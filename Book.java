54fdfbf57fa985d50b612335642e20bb627cb486
Nicolas M <43154687+Nikolasmej@users.noreply.github.com>
2018-09-22 15:38:21 -0400

    Exercise  2.84, 2.85, 2.87 & 2.88
    Exercise  2.84, 2.85, 2.87 & 2.88

--- Book.java
+++ Book.java
@@ -11,31 +11,92 @@ class Book
     // The fields.
     private String author;
     private String title;
-
+    //Ex 2.85
+    private int pages;
+    // EX 2.88
+    private String refNumber;
     /**
-     * exercise 2.83
+     
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle,int numberPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        // ex 2.85
+        pages = numberPages;
+        // EX 2.88
+        refNumber = "";
+        
     }
 
     // Add the methods here ...
-    /**
-     * Accessor method to return author filed
+    /** exercise 2.83
+     * Accessor method to return author field
      */
     public String getAuthor()
     {
      return author;   
     }
+    
     /**
-     * Accessor method to return title filed
+     * Accessor method to return title field
      */
     public String getTitle()
     {
      return title;   
     }
+    
+    /** exercise 2.84
+     *  method to print author 
+     */
+    public void printAuthor()
+    {
+        System.out.println("Author: " + author);
+    }
+    
+    /** exercise 2.84
+     *  method to print title 
+     */
+    public void printTitle()
+    {
+        System.out.println("Title: " + title);
+    }
+    
+    /**
+     * ex 2.85 method to return # of pages
+     */
+    public int getPages()
+    {
+     return pages;   
+    }
+    
+    /** exercise 2.87
+     *  method to print details of the book 
+     */
+    public void printDetails()
+    {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+    }
+    
+    /**
+     * ex 2.88 method to set ref # of page
+     */
+    public void setRefNumber(String ref)
+    {
+        refNumber = ref;  
+    }
+    
+    /**
+     * ex 2.88 method to get ref # of page
+     */
+    public String getrefNumber()
+    {
+        return refNumber;  
+    }
+    
+    
 }
