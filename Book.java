7a14a5e8f5edac181d77e5f61b28070cbad4c793
Nicolas M <43154687+Nikolasmej@users.noreply.github.com>
2018-09-23 10:49:39 -0400

    Exercise  2.91 & 2.92
    Exercise  2.91 & 2.92 with previous commit

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author (Nicolas M.)
+ * @version (9.22.2018.)
  */
 class Book
 {
